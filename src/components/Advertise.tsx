5f0e9f427ab59776d3a9ed713c95ca3a4910b2a1
sumon-hossain <sumonengr.96@gmail.com>
2023-02-08 17:12:33 +0600

    2nd Commit

--- src/components/Advertise.tsx
+++ src/components/Advertise.tsx
@@ -1,8 +1,17 @@
-const Advertise = () => {
+const Advertise = ({ brands }: any) => {
   return (
-    <div>
-      <h1>Advertise</h1>
-    </div>
+    <>
+      <div className="my-16 lg:my-7 w-9/12 lg:w-[85vw] m-auto flex items-center justify-between xl:gap-7 overflow-x-scroll scroll-smooth scroll-hidden">
+        {brands?.map((val: any, i: any) => (
+          <img
+            key={i}
+            src={val.iconSrc}
+            alt="brands/icon"
+            className="w-44 h-auto object-fill xl:w-38 lg:w-32 hover:scale-105 transition-all duration-300 cursor-pointer filter drop-shadow-md"
+          />
+        ))}
+      </div>
+    </>
   );
 };
 
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,9 @@
 export { default as Navbar } from ".//Navbar";
 export { default as Hero } from "./Hero";
 export { default as Footer } from "./Footer";
+export { default as Memory } from "./Memory";
+export { default as Explore } from "./Explore";
+export { default as Banner } from "./Banner";
+export { default as Newslatter } from "./Newslatter";
+export { default as Advertise } from "./Advertise";
+export { default as Pricings } from "./Pricings";
